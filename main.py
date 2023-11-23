438afc315c60e2f0c6344264a37449ef8dd3cf2b
MMDOV <mohammadvgamer@gmail.com>
2023-11-23 17:31:59 +0330

    The first version of the UI is done

---
+++ main.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.chrome import service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchWindowException
+import time
+import ttkbootstrap as ttk
+
+
+# webdriver_path = r"chromedriver.exe"
+# ser = service.Service(executable_path=webdriver_path)
+# option = Options()
+# driver = webdriver.Chrome(service=ser, options=option)
+#
+#
+# # Starts the code and opens the webpage
+# def start_the_process():
+#     global driver
+#     driver.get("")
+#
+
+def validate_number(x) -> bool:
+    """Validates that the input is a number"""
+    if x.isdigit() and len(x) == 11:
+        return True
+    elif x == "":
+        return True
+    else:
+        return False
+
+
+# ----------------------------------------------UI------------------------------------------------ #
+window = ttk.Window()
+window.title("ربات گرفتن نوبت صرافی")
+window.config(pady=20, padx=20)
+digit_func = window.register(validate_number)
+name_and_last_name_label = ttk.Label(text="نام و نام خانوادگی ", padding=10)
+name_and_last_name_label.grid(row=0, column=1)
+name_and_last_name_entry = ttk.Entry(width=20)
+name_and_last_name_entry.grid(row=0, column=0)
+phone_number_label = ttk.Label(text="شماره موبایل", padding=10)
+phone_number_label.grid(row=1, column=1)
+phone_number_entry = ttk.Entry(width=20, validate="focus", validatecommand=(digit_func, '%P'))
+phone_number_entry.grid(row=1, column=0)
+number_of_appointment_label = ttk.Label(text="تعداد نوبت ها", padding=10)
+number_of_appointment_label.grid(row=2, column=1)
+number_of_appointment_entry = ttk.Entry(width=20)
+number_of_appointment_entry.grid(row=2, column=0)
+button = ttk.Button(text="شروع", width=20, bootstyle='dark')
+button.config(padding=10)
+button.grid(row=3, column=0, columnspan=2)
+window.mainloop()
