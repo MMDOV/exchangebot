e0a21e9af6b7c02b24c6c62a5060d980f2521010
MMDOV <mohammadvgamer@gmail.com>
2023-11-24 11:31:17 +0330

    The real first testable version

--- main.py
+++ main.py
@@ -1,23 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.chrome import service
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchWindowException
 import time
 import ttkbootstrap as ttk
+from random import choice
+
+webdriver_path = r"chromedriver.exe"
+ser = service.Service(executable_path=webdriver_path)
+option = Options()
+capa = DesiredCapabilities.CHROME
+capa["pageLoadStrategy"] = "none"
+driver = webdriver.Chrome(service=ser, options=option, desired_capabilities=capa)
+wait = WebDriverWait(driver, 20)
 
 
-# webdriver_path = r"chromedriver.exe"
-# ser = service.Service(executable_path=webdriver_path)
-# option = Options()
-# driver = webdriver.Chrome(service=ser, options=option)
-#
-#
-# # Starts the code and opens the webpage
-# def start_the_process():
-#     global driver
-#     driver.get("")
-#
+# Starts the code and opens the webpage
+def start_the_process():
+    global driver
+    driver.get(r"file:///V:/PycharmProjects/bot-sarafi/page_1.html")
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "option[value = '0']")))
+    driver.execute_script("window.stop();")
+    select_element = Select(driver.find_element(By.XPATH, r"/html/body/div[1]/div/div[2]/div/div/article/div/div/div["
+                                                          r"2]/div[2]/div[1]/div/div/div/select"))
+    select_element.select_by_value('1')
+    next_button = driver.find_element(By.CLASS_NAME, 'bookly-next-step')
+    # next_button.click() Temporary
+    driver.get(r"file:///V:/PycharmProjects/bot-sarafi/page_2.html")  # Temporary
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'bookly-hour')))
+    driver.execute_script("window.stop();")
+    book_column = driver.find_element(By.CLASS_NAME, r'bookly-column')
+    available_times = []
+    for available_time in book_column.find_elements(By.CLASS_NAME, 'bookly-hour'):
+        if available_time.is_enabled():
+            available_times.append(available_time)
+    choice(available_times).click()
+    driver.get(r"file:///V:/PycharmProjects/bot-sarafi/%D8%B3%D8%A7%D9%85%D8%A7%D9%86%D9%87%20%D9%86%D9%88%D8%A8%D8"
+               r"%AA%20%D8%AF%D9%87%DB%8C%20%D8%B5%D8%B1%D8%A7%D9%81%DB%8C%20%D8%AF%D9%88%D9%84%D8%AE%D8%A7%D9%86%DB"
+               r"%8C%20%E2%80%93%20%D8%B5%D8%B1%D8%A7%D9%81%DB%8C%20%D8%AF%D9%88%D9%84%D8%AE%D8%A7%D9%86%DB%8C3.html")
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, r'bookly-js-full-name')))
+    driver.execute_script("window.stop();")
+    name_input = driver.find_element(By.CLASS_NAME, r'bookly-js-full-name')
+    name_input.clear()
+    name_input.send_keys("نام تستی")
+    phone_number_input = driver.find_element(By.CLASS_NAME, r'bookly-js-user-phone-input')
+    phone_number_input.clear()
+    phone_number_input.send_keys("11111111111")
+
 
 def validate_phone_number(x) -> bool:
     """Validates that the input is a phone number"""
@@ -57,7 +90,7 @@ number_of_appointment_label = ttk.Label(text="تعداد نوبت ها", padding
 number_of_appointment_label.grid(row=2, column=1)
 number_of_appointment_entry = ttk.Entry(width=20, validate="focus", validatecommand=(digit_func, '%P'))
 number_of_appointment_entry.grid(row=2, column=0)
-button = ttk.Button(text="شروع", width=20, bootstyle='dark')
+button = ttk.Button(text="شروع", width=20, bootstyle='dark', command=start_the_process)
 button.config(padding=10)
 button.grid(row=3, column=0, columnspan=2)
 window.mainloop()
