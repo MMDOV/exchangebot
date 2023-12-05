f55a3fcc8d97512d62ae6bc6775d6ea4c5aeff8f
MMDOV <mohammadvgamer@gmail.com>
2023-12-05 12:02:09 +0330

    some tweaks to captcha

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class Main:
         option.page_load_strategy = "none"
         self.driver = webdriver.Chrome(service=ser, options=option)
         self.wait = WebDriverWait(self.driver, 20)
-        self.first_step()
+        self.third_step()
 
     def first_step(self):
         """
@@ -146,7 +146,16 @@ class Main:
             phone_number_input.send_keys(self.phone_number)
             captcha_input = self.driver.find_element(By.CLASS_NAME, r'bookly-captcha')
             captcha_input.click()
-
+            while True:
+                length_of_captcha = len(captcha_input.get_attribute("value"))
+                if length_of_captcha == 5:
+                    break
+            next_button = self.driver.find_element(By.CLASS_NAME, 'bookly-next-step')
+            next_button.click()
+            try:
+                next_button.click()
+            except NoSuchElementException:
+                pass
             while True:
                 try:
                     self.wait.until(ec.invisibility_of_element_located((By.CLASS_NAME, r'bookly-captcha')))
