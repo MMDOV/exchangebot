5cd69e6b64272ae362f57d7336360e6926d7fad3
MMDOV <mohammadvgamer@gmail.com>
2023-12-24 15:07:05 +0330

    small tweaks

--- main.py
+++ main.py
@@ -207,7 +207,8 @@ class MainProcess:
                 except TimeoutException:
                     continue
 
-            self.driver.find_element(By.CLASS_NAME, r'latepoint-next-btn').click()
+            next_button = self.driver.find_element(By.CLASS_NAME, r'latepoint-next-btn')
+            next_button.click()
 
         except NoSuchWindowException:
             messagebox.showerror(message="!پنجره مورد نظر بسته شده و یا وجود ندارد", title=f'{self.index} پنجره ')
