2090c7438edd72c4c1eea3340e7c1df04f5e0aab
MMDOV <mohammadvgamer@gmail.com>
2024-01-05 19:24:51 +0330

    client was dumb it was working going back again but on master...

--- main.py
+++ main.py
@@ -206,7 +206,12 @@ class MainProcess:
                 except TimeoutException:
                     continue
 
-            self.driver.find_element(By.CLASS_NAME, r'latepoint-next-btn').click()
+            while True:
+                try:
+                    next_button = self.driver.find_element(By.CLASS_NAME, r'latepoint-next-btn')
+                    next_button.click()
+                except NoSuchElementException:
+                    break
 
         except NoSuchWindowException:
             Messagebox.show_error(message="!پنجره مورد نظر بسته شده و یا وجود ندارد", title=f'{self.index} پنجره ')
